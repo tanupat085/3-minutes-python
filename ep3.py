f0835e62a276f94811370e9275ce851c92609b57
tanupat085 <54698281+tanupat085@users.noreply.github.com>
2020-07-16 21:16:59 +0700

    ep3

---
+++ ep3.py
@@ -0,0 +1,18 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> position = (300,500)
+>>> print(position[0])
+300
+>>> print(position[1])
+500
+>>> x,y = (300,500)
+>>> print(x)
+300
+>>> print(y)
+500
+>>> car = {'Honda':'civic','Toyota':'yaris'}
+>>> print(car['Honda'])
+civic
+>>> print(car['Toyota'])
+yaris
+>>> 
